58a6c03dfe9b86c5cb231f5737a0154eafb7923e
Himanshu Choudhary <himanshuchoudhary@live.com>
2016-10-12 15:29:49 +0530

    Changed p1_d. Using UnigramTagger to train the nltk tagger & test

--- p1_d.py
+++ p1_d.py
@@ -5,32 +5,32 @@
 from __future__ import print_function
 import numpy as np
 
-from Utils import getBrownDatasetTokens
+from Utils import getBrownDatasetAsItIs
 
-from sklearn.metrics import accuracy_score
+from sklearn.cross_validation import train_test_split
 
-from nltk import pos_tag
+from nltk.tag import str2tuple
+from nltk.tag import UnigramTagger
 
-def tag(token):
-    return pos_tag([token])[0][1].lower()
+def prepareForNLTK(sentence):
+    return [str2tuple(word) for word in sentence.lower().split()]
 
 np.random.seed(1)
 
 FILE_OUT = open("output/p1_d.txt", 'a')
 
-dataset = getBrownDatasetTokens()
+data = getBrownDatasetAsItIs().sentence.map(prepareForNLTK)
+dataset = []
+for x in data:
+    dataset.append(x)
 
-tokens = dataset.index
-tags = dataset['tag'].values
+train_data, test_data = train_test_split(dataset,test_size=.2)
 
-nltk_tags = tokens.map(tag)
+nltk_tagger = UnigramTagger(train_data)
 
-tagset = list(set(tags))
-nltk_tagset = list(set(nltk_tags))
-
-score = accuracy_score(tags, nltk_tags)
-print("Total (token,tag) pairs in the dataset : {:d}".format(len(dataset)), file=FILE_OUT)
-print("Tagset size : {:d}".format(len(tagset)), file=FILE_OUT)
-print("NLTK tagset size : {:d}".format(len(nltk_tagset)), file=FILE_OUT)
+score = nltk_tagger.evaluate(test_data)
+print("Total sentences in the dataset : {:d}".format(len(dataset)), file=FILE_OUT)
+print("# of sentences used as training set : {:d}".format(len(train_data)), file=FILE_OUT)
+print("# of sentences used as testing set : {:d}".format(len(test_data)), file=FILE_OUT)
 print("Accuracy : {:.2f}%".format(score*100), file=FILE_OUT)
 print("\n", file=FILE_OUT)
